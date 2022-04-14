a3a747e6eadde1ba3b99113f7cab33d52c58e36c
otsokeuda <otso.rantala@edu.keuda.fi>
2022-04-14 19:08:26 +0300

    drag

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     
     setComponents([...components, ""]) 
     
-  } 
+  }   
   
   return ( 
     
--- src/Note.js
+++ src/Note.js
@@ -1,18 +1,21 @@
 import React from 'react'; 
+import Draggable from "react-draggable";
+import { RiDragMove2Fill } from "react-icons/ri";
 
 const Note = (props) => {
 return ( 
     
     <div   className="Component"> 
     
-        <textarea className='style'></textarea>
+    <Draggable>        
+        <div>
+            <textarea className='style'></textarea>
+        </div>
+    </Draggable>         
     
     </div>
     
-    
-  ); 
-  
+  );   
 }; 
-
 
 export {Note};
