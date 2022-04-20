d403588f5172be91a101fb5396621280ae02d523
otsokeuda <otso.rantala@edu.keuda.fi>
2022-04-20 20:09:05 +0300

    trash

--- src/Note.js
+++ src/Note.js
@@ -1,15 +1,30 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import Draggable from "react-draggable";
 import { RiDragMove2Fill } from "react-icons/ri";
+import { FiTrash2 } from "react-icons/fi";
+
 
 const Note = (props) => {
+
+    function removeComponent () {
+        alert()
+    }
+    
 return ( 
     
-    <div   className="Component"> 
+    <div className="Component"> 
     
-    <Draggable>        
+    <Draggable handle=".handle">       
         <div>
+        
+            <div className="handle"><RiDragMove2Fill className='pic'/>
+            
+            <button className='remove' onClick={removeComponent}><FiTrash2 className='trash'/></button>
+            
+            </div>
+
             <textarea className='style'></textarea>
+            
         </div>
     </Draggable>         
     
